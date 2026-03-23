8981ac030f778a210e6ca6665b19f85313d0344e
Gayathiri V <52602000+Gayathiri18@users.noreply.github.com>
2026-03-23 12:32:24 +0530

    GLS script
    Added comments for clarity and organization of the script.

--- gls/run_gls.sh
+++ gls/run_gls.sh
@@ -5,9 +5,16 @@ rm -rf xrun.*
 
 echo "🚀 Running Gate-Level Simulation (GLS)..."
 
+# RTL Verification
 xrun -gui -access +rwc \
+/home/super/circuits/virtuoso_test/rtl2gds/rtl/counter.v  /home/super/circuits/virtuoso_test/rtl2gds/rtl/counter_tb.v 
+
+# After Synthesis Run 
+#xrun -gui -access +rwc \
 /home/super/circuits/virtuoso_test/rtl2gds/synthesis/output/counter_netlist.v counter_tb.v \
 -v /home/install/FOUNDRY/digital/90nm/dig/vlog/slow.v
+
+
 
 #slow.v - Verilog model file of standard cells, Behavioral definitions of all gates used in the synthesized netlist
 
